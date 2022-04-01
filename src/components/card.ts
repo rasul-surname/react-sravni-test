becbe16b5c2585897cbc5ab9ff42f07d643fe15b
rasul-surname <rasul.surname@mail.ru>
2022-04-01 13:18:33 +0300

    реализовал запрос через action-creator

---
+++ src/components/card.ts
@@ -0,0 +1,24 @@
+import {CardAction, CardActionTypes} from "../types/card";
+import {Dispatch} from "redux";
+
+
+export const fetchCards = () => {
+    return async (dispatch: Dispatch<CardAction>) => {
+        try {
+            dispatch({type: CardActionTypes.FETCH_CARDS});
+            const response = await fetch("http://localhost:3001/data");
+            const payload = await response.json();;
+            setTimeout(() => {
+                dispatch({
+                    type: CardActionTypes.FETCH_CARDS_SUCCESS,
+                    payload: payload,
+                });
+            }, 1000)
+        } catch (e) {
+            dispatch({
+                type: CardActionTypes.FETCH_CARDS_ERROR,
+                payload: 'Произошла ошибка при загрузке данных'
+            });
+        }
+    }
+}
