c1cd6f112350f968a694d645c3aac1f0ee0d9636
rasul-surname <rasul.surname@mail.ru>
2022-04-04 12:02:04 +0300

    изменил логику вызова данных с json-servera на статичный файл

--- src/store/action-creators/card.ts
+++ src/store/action-creators/card.ts
@@ -1,18 +1,27 @@
 import {CardAction, CardActionTypes} from "../../types/card";
 import {Dispatch} from "redux";
-
+import payload from './data';
 
 export const fetchCards = () => {
     return async (dispatch: Dispatch<CardAction>) => {
         try {
-            const response = await fetch("http://localhost:3001/data");
-            const payload = await response.json();;
+            // Вызов через json-server + задержка для эмуляции работы сервера
+            // const response = await fetch("http://localhost:3001/data");
+            // const payload = await response.json();
+            // setTimeout(() => {
+            //     dispatch({
+            //         type: CardActionTypes.FETCH_CARDS_SUCCESS,
+            //         payload: payload,
+            //     });
+            // }, 1000);
+
+            // Вызов через статичный файл + задержка для эмуляции работы сервера
             setTimeout(() => {
-                dispatch({
-                    type: CardActionTypes.FETCH_CARDS_SUCCESS,
-                    payload: payload,
-                });
-            }, 1000)
+                    dispatch({
+                        type: CardActionTypes.FETCH_CARDS_SUCCESS,
+                        payload: payload,
+                    });
+                }, 1000);
         } catch (e) {
             dispatch({
                 type: CardActionTypes.FETCH_CARDS_ERROR,
